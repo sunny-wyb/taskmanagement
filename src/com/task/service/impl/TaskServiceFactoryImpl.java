de91209450d52085637c97d50e68ae0691bf17f5
sunny-wyb <zjuvpnwyb@zju.edu.cn>
2013-02-02 21:44:54 +0800

    delete test

--- src/com/task/service/impl/TaskServiceFactoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.task.service.impl;
-
-import com.task.service.TaskService;
-
-public class TaskServiceFactoryImpl{
-	public static TaskService getTaskService() {
-		return new TaskServiceImpl();
-	}
-}
